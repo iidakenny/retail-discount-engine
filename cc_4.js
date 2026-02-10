91793678eb97ec61fa6c44c96f5d458d2952e8e3
Iida Kenny <iida@usf.edu>
2026-02-10 13:09:25 -0500

    Created the product invenvory with 5 items and their information.

---
+++ cc_4.js
@@ -0,0 +1,36 @@
+// A list of 5 products with their name, category, price, and quantity in inventory.
+let products = [
+
+    {
+        name: "TV",
+        category: "electronics",
+        price: 89.99,
+        inventory: 32
+    },
+    {
+        name: "T-shirt",
+        category: "apparel",
+        price: 15.99,
+        invenroty: 14
+    },
+    {
+        name: "Banana",
+        category:"groceries",
+        price: 0.99,
+        inventory: 48
+    },
+    {
+        name: "Sofa",
+        category: "household",
+        price: 599.99,
+        inventory: 3
+    },
+    {
+        name: "Yarn"
+        category: "crafts",
+        price: 6.99
+        inventory: 200
+    }
+];
+
+
