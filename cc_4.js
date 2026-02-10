85be039975ec78eb574879251cd7f1ce1de54eab
Iida Kenny <iida@usf.edu>
2026-02-10 13:42:46 -0500

    Created a dynamic discount loop and made a console.log to check if the discounting worked. Also fixed the price to 2 decimals in the console output.

--- cc_4.js
+++ cc_4.js
@@ -1,4 +1,5 @@
 // A list of 5 products with their name, category, price, and quantity in inventory.
+
 let products = [
 
     {
@@ -11,7 +12,7 @@ let products = [
         name: "T-shirt",
         category: "apparel",
         price: 15.99,
-        invenroty: 14
+        inventory: 14
     },
     {
         name: "Banana",
@@ -26,11 +27,44 @@ let products = [
         inventory: 3
     },
     {
-        name: "Yarn"
+        name: "Wool yarn, 100g",
         category: "crafts",
-        price: 6.99
+        price: 6.99,
         inventory: 200
     }
 ];
 
+// checking if products are correct on console.
 
+console.log("My inventory:");
+products.forEach(product => {
+    console.log(`${product.name} | ${product.category} | $${product.price} | ${product.inventory}`);
+});
+
+
+// For... of loop with switch inside to apply a dynamic discount based on product category. -20% electornics, -15% apparel, groceries/household -10%.
+
+for (let product of products){
+    switch (product.category){
+        case "electronics":
+            product.price*=(1-0.2);
+            break;
+
+        case "apparel":
+            product.price*=(1-0.15);
+            break;
+
+        case "groceries":
+        case "household":
+            product.price*=(1-0.1);
+            break;
+        default:
+            product.price = product.price
+}}
+
+// Checking is discounts were applied correctly
+
+console.log("\nPrices after discounts:");
+products.forEach(product => {
+    console.log(`${product.name} | ${product.category} | $${product.price.toFixed(2)}`);
+    });
