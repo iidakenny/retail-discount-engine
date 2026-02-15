4a8affa9eea5433b61601fd2d696fc38f1cde074
Iida Kenny <iida@usf.edu>
2026-02-15 17:52:57 -0500

    Created a loop for processing customer orders and logged each order with their total on console. Also created a summry of the keys in the first product in inventory.

--- cc_4.js
+++ cc_4.js
@@ -89,6 +89,10 @@ if (customerType === "student"){
 //Cheking if the student discount is applied properly.
 console.log(`\nYour order total is $${orderTotal.toFixed(2)} after applying the ${customerType} discount.`)
 
+
+
+// Created a list of 3 different customer orders with varying items in the cart.
+
 let customers = [
     {
         order: 1,
@@ -99,7 +103,7 @@ let customers = [
                 quantity: 2
             },
             {
-                product: "T-Shirt",
+                product: "T-shirt",
                 quantity: 3
             }
 
@@ -111,7 +115,7 @@ let customers = [
         cart: [
             {
                 product: "Sofa",
-                quantity: 5
+                quantity: 1
             },
             {
                 product: "Wool yarn, 100g",
@@ -131,4 +135,40 @@ let customers = [
     }
 
 ];
+
+//Looping throught the customer order while checking the invenotry and substr. the bought amount, calculating the total with discounts, and logging the customer number and order toal.
+
+for (let customer of customers) {
+    let orderTotal = 0;
+
+    for (let item of customer.cart) {
+        let product = products.find(p => p.name === item.product);
+
+        if (product && product.inventory >= item.quantity) {
+            orderTotal += product.price * item.quantity;
+            product.inventory -= item.quantity;
+        }
+        else {
+            console.log(`Inventory too low for product: ${item.product}`)
+        }
+    }
+
+    if (customerType === "student"){
+        orderTotal*=studentDiscount;
+    } else if (customerType === "senior"){
+        orderTotal*=seniorDiscount;
+    } else {
+        orderTotal = orderTotal;
+    }
+
+    console.log(`Order number: ${customer.order}, Total after discounts: $${orderTotal.toFixed(2)}`)
+};
+
+// A summary of the key's and their values of a single product
+console.log("\nSummary of the first product in inventory:")
+ for (let key in products[0]) {
+    console.log(`${key}: ${products[0][key]}`)
+ }
+
+ 
 
